1a6770778d858284a3d05246ee1dbed81a9da042
Abdullah Barhoum <ka70911@gmail.com>
2022-09-07 19:34:49 +0200

    Updates

--- AUTOMATIC1111/mount.sh
+++ AUTOMATIC1111/mount.sh
@@ -23,6 +23,10 @@ ln -s -T /models /opt/conda/lib/python3.7/site-packages/realesrgan/weights
 # force facexlib cache
 mkdir -p /cache/weights/ ${WORKDIR}/gfpgan/
 ln -sf /cache/weights/ ${WORKDIR}/gfpgan/
+# code former cache
+rm -rf ${ROOT}/repositories/CodeFormer/weights/CodeFormer ${ROOT}/repositories/CodeFormer/weights/facelib
+ln -sf -T /cache/weights ${ROOT}/repositories/CodeFormer/weights/CodeFormer
+ln -sf -T /cache/weights ${ROOT}/repositories/CodeFormer/weights/facelib
 
 # mount config
 ln -sf /docker/config.json ${WORKDIR}/config.json
