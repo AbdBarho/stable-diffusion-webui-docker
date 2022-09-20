a96285d10bc875a29287fc71e04948f23915b815
Abdullah Barhoum <ka70911@gmail.com>
2022-09-20 19:35:10 +0200

    Update License

--- services/download/download.sh
+++ services/download/download.sh
@@ -4,6 +4,13 @@ set -Eeuo pipefail
 
 mkdir -p /cache/torch /cache/transformers /cache/weights /cache/models /cache/custom-models
 
+cat <<EOF
+By using this software, you agree to the following licenses:
+https://github.com/CompVis/stable-diffusion/blob/main/LICENSE
+https://github.com/TencentARC/GFPGAN/blob/master/LICENSE
+https://github.com/xinntao/Real-ESRGAN/blob/master/LICENSE
+EOF
+
 echo "Downloading, this might take a while..."
 
 aria2c --input-file /docker/links.txt --dir /cache/models --continue
