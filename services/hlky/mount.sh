43a5e5e85fb6306e9a6d7c7801335a1e9ee17fd5
AbdBarho <ka70911@gmail.com>
2022-09-26 08:31:47 +0200

    Update versions (#99)
    - auto:
    https://github.com/AUTOMATIC1111/stable-diffusion-webui/commit/ca3e5519e8b6dc020c5e7ae508738afb5dc6f3ec
    - hlky:
    https://github.com/sd-webui/stable-diffusion-webui/commit/1fd28eed1ebc3aa04b9b00e2a899f3bf07f64bdc
    - lstein:
    https://github.com/lstein/stable-diffusion/commit/b40bfb5116b7fc618f78a0d152005ceb46153443

--- services/hlky/mount.sh
+++ services/hlky/mount.sh
@@ -9,8 +9,8 @@ ROOT=/stable-diffusion/src
 MODELS["${ROOT}/gfpgan/experiments/pretrained_models/GFPGANv1.3.pth"]=GFPGANv1.3.pth
 MODELS["${ROOT}/realesrgan/experiments/pretrained_models/RealESRGAN_x4plus.pth"]=RealESRGAN_x4plus.pth
 MODELS["${ROOT}/realesrgan/experiments/pretrained_models/RealESRGAN_x4plus_anime_6B.pth"]=RealESRGAN_x4plus_anime_6B.pth
-MODELS["/latent-diffusion/experiments/pretrained_models/model.ckpt"]=LDSR.ckpt
-# MODELS["/latent-diffusion/experiments/pretrained_models/project.yaml"]=LDSR.yaml
+MODELS["${ROOT}/latent-diffusion/experiments/pretrained_models/model.ckpt"]=LDSR.ckpt
+MODELS["${ROOT}/latent-diffusion/experiments/pretrained_models/project.yaml"]=LDSR.yaml
 
 MODELS_DIR=/cache/models
 
@@ -24,11 +24,6 @@ for path in "${!MODELS[@]}"; do
     echo "Skipping ${name}"
   fi
 done
-
-# hack for latent-diffusion
-if test -f "${MODELS_DIR}/LDSR.yaml"; then
-  sed 's/ldm\./ldm_latent\./g' "${MODELS_DIR}/LDSR.yaml" >/latent-diffusion/experiments/pretrained_models/project.yaml
-fi
 
 # force facexlib cache
 mkdir -p /cache/weights/ /stable-diffusion/gfpgan/
--- services/lstein/mount.sh
+++ services/lstein/mount.sh
@@ -7,12 +7,11 @@ ROOT=/stable-diffusion
 mkdir -p "${ROOT}/models/ldm/stable-diffusion-v1/"
 ln -sf /cache/models/model.ckpt "${ROOT}/models/ldm/stable-diffusion-v1/model.ckpt"
 
-if test -f /cache/models/GFPGANv1.3.pth; then
-  base="${ROOT}/src/gfpgan/experiments/pretrained_models/"
-  mkdir -p "${base}"
-  ln -sf /cache/models/GFPGANv1.3.pth "${base}/GFPGANv1.3.pth"
-  echo "Mounted GFPGANv1.3.pth"
-fi
+base="${ROOT}/src/gfpgan/experiments/pretrained_models/"
+mkdir -p "${base}"
+# TODO: "real" GFPGANv1.4.pth
+ln -sf /cache/models/GFPGANv1.3.pth "${base}/GFPGANv1.4.pth"
+echo "Mounted GFPGANv1.3.pth"
 
 # facexlib
 FACEX_WEIGHTS=/opt/conda/lib/python3.9/site-packages/facexlib/weights
