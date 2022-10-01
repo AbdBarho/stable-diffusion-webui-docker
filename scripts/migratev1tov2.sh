5fa96a50ff69ebcc5763b0b2e2e10b70430239e8
AbdBarho <ka70911@gmail.com>
2022-10-01 12:27:13 +0200

    verbose copy

--- scripts/migratev1tov2.sh
+++ scripts/migratev1tov2.sh
@@ -1,27 +1,29 @@
-mkdir -p data/.cache data/StableDiffusion data/Codeformer data/GFPGAN data/ESRGAN data/BSRGAN data/RealESRGAN data/SwinIR data/LDSR
+mkdir -p data/.cache data/StableDiffusion data/Codeformer data/GFPGAN data/ESRGAN data/BSRGAN data/RealESRGAN data/SwinIR data/LDSR /data/embeddings
 
-cp -f cache/models/model.ckpt data/StableDiffusion/model.ckpt
+cp -vf cache/models/model.ckpt data/StableDiffusion/model.ckpt
 
-cp -f cache/models/LDSR.ckpt data/LDSR/model.ckpt
-cp -f cache/models/LDSR.yaml data/LDSR/project.yaml
+cp -vf cache/models/LDSR.ckpt data/LDSR/model.ckpt
+cp -vf cache/models/LDSR.yaml data/LDSR/project.yaml
 
-cp -f cache/models/RealESRGAN_x4plus.pth data/RealESRGAN/
-cp -f cache/models/RealESRGAN_x4plus_anime_6B.pth data/RealESRGAN/
+cp -vf cache/models/RealESRGAN_x4plus.pth data/RealESRGAN/
+cp -vf cache/models/RealESRGAN_x4plus_anime_6B.pth data/RealESRGAN/
 
-cp -rf cache/torch data/.cache/
+cp -vrf cache/torch data/.cache/
 
 mkdir -p data/.cache/huggingface/transformers/
-cp -rf cache/transformers/* data/.cache/huggingface/transformers/
+cp -vrf cache/transformers/* data/.cache/huggingface/transformers/
 
-cp cache/custom-models/* data/StableDiffusion/
+cp -v cache/custom-models/* data/StableDiffusion/
 
 mkdir -p data/.cache/clip/
-cp -f cache/weights/ViT-L-14.pt data/.cache/clip/
+cp -vf cache/weights/ViT-L-14.pt data/.cache/clip/
 
-cp -f cache/weights/codeformer.pth data/Codeformer/codeformer-v0.1.0.pth
+cp -vf cache/weights/codeformer.pth data/Codeformer/codeformer-v0.1.0.pth
 
-cp -f cache/weights/detection_Resnet50_Final.pth data/.cache/
-cp -f cache/weights/parsing_parsenet.pth data/.cache/
+cp -vf cache/weights/detection_Resnet50_Final.pth data/.cache/
+cp -vf cache/weights/parsing_parsenet.pth data/.cache/
+
+cp -v embeddings/* data/embeddings/
 
 echo Dont forget to run: docker compose --profile download up --build
-echo the old cache folder can be deleted.
+echo the cache and embeddings folders can be deleted, but its not necessary.
