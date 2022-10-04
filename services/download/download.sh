5c397b3a30f58f2423b4eeed729ed815778d5c4b
AbdBarho <ka70911@gmail.com>
2022-10-04 19:06:34 +0200

    data

--- services/download/download.sh
+++ services/download/download.sh
@@ -2,7 +2,7 @@
 
 set -Eeuo pipefail
 
-mkdir -p /data/.cache /data/StableDiffusion /data/Codeformer /data/GFPGAN /data/ESRGAN /data/BSRGAN /data/RealESRGAN /data/SwinIR /data/LDSR
+mkdir -p /data/.cache /data/StableDiffusion /data/Codeformer /data/GFPGAN /data/ESRGAN /data/BSRGAN /data/RealESRGAN /data/SwinIR /data/LDSR /data/ScuNET /data/embeddings
 
 cat <<EOF
 By using this software, you agree to the following licenses:
