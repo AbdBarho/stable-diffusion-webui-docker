42545f6a3cef0ddec0fba85748136b123c91575c
pirahtays <35934562+pirahtays@users.noreply.github.com>
2022-10-26 11:45:06 -0400

    Update optional-deepdanbooru.sh

--- services/AUTOMATIC1111/optional-deepdanbooru.sh
+++ services/AUTOMATIC1111/optional-deepdanbooru.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip install tensorflow==2.10
+pip install tensorflow-cpu==2.10
 pip install tensorflow-io==0.27.0 
 pip install git+https://github.com/KichangKim/DeepDanbooru.git@edf73df4cdaeea2cf00e9ac08bd8a9026b7a7b26#egg=deepdanbooru deepdanbooru
