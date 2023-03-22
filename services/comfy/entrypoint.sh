1c6d295fb3c417bcd36f091607a5bcd5dbadbee7
Jonathan Kovacs <jkovacs-dev@users.noreply.github.com>
2023-03-22 17:52:42 -0400

    Map to existing auto and invoke folder structures

--- services/comfy/entrypoint.sh
+++ services/comfy/entrypoint.sh
@@ -12,12 +12,14 @@ MOUNTS["/root/.cache"]=/data/.cache
 MOUNTS["${ROOT}/models/checkpoints"]="/data/StableDiffusion"
 MOUNTS["${ROOT}/models/clip"]="/data/comfy/clip"
 MOUNTS["${ROOT}/models/clip_vision"]="/data/comfy/clip_vision"
-MOUNTS["${ROOT}/models/controlnet"]="/data/comfy/controlnet"
+MOUNTS["${ROOT}/models/controlnet"]="/data/config/auto/extensions/sd-webui-controlnet/models"
 MOUNTS["${ROOT}/models/embeddings"]="/data/embeddings"
 MOUNTS["${ROOT}/models/loras"]="/data/Lora"
 MOUNTS["${ROOT}/models/style_models"]="/data/comfy/style_models"
 MOUNTS["${ROOT}/models/t2i_adapter"]="/data/comfy/t2i_adapter"
-MOUNTS["${ROOT}/models/upscale_models"]="/data/comfy/upscale_models"
+MOUNTS["${ROOT}/models/upscale_models/RealESRGAN"]="/data/RealESRGAN"
+MOUNTS["${ROOT}/models/upscale_models/SwinIR"]="/data/SwinIR"
+MOUNTS["${ROOT}/models/upscale_models/GFPGAN"]="/data/GFPGAN"
 MOUNTS["${ROOT}/models/vae"]="/data/VAE"
 
 MOUNTS["${ROOT}/output"]="/output/comfy"
@@ -26,7 +28,10 @@ for to_path in "${!MOUNTS[@]}"; do
   set -Eeuo pipefail
   from_path="${MOUNTS[${to_path}]}"
   rm -rf "${to_path}"
-  mkdir -p "$(dirname "${to_path}")"
+  if [ ! -f "$from_path" ]; then
+    mkdir -vp "$from_path"
+  fi
+  mkdir -vp "$(dirname "${to_path}")"
   ln -sT "${from_path}" "${to_path}"
   echo Mounted $(basename "${from_path}")
 done
