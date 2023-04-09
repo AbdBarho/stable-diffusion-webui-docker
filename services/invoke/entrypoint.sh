a76aeba4f45bf1e33eb0c1ddb169e742f043a749
AbdBarho <ka70911@gmail.com>
2023-04-09 22:33:18 +0200

    move entirely to HF

--- services/invoke/entrypoint.sh
+++ services/invoke/entrypoint.sh
@@ -7,22 +7,23 @@ declare -A MOUNTS
 # cache
 MOUNTS["/root/.cache"]=/data/.cache/
 
+# this is really a hack to avoid migrations
+rm -rf ${HF_HOME}/diffusers
+
 # ui specific
 MOUNTS["${ROOT}/models/codeformer"]=/data/Codeformer/
-
 MOUNTS["${ROOT}/models/gfpgan/GFPGANv1.4.pth"]=/data/GFPGAN/GFPGANv1.4.pth
-MOUNTS["${ROOT}/models/gfpgan/weights"]=/data/.cache/
-
+MOUNTS["${ROOT}/models/gfpgan/weights"]=/data/GFPGAN/
 MOUNTS["${ROOT}/models/realesrgan"]=/data/RealESRGAN/
 
 MOUNTS["${ROOT}/models/bert-base-uncased"]=/data/.cache/huggingface/transformers/
 MOUNTS["${ROOT}/models/openai/clip-vit-large-patch14"]=/data/.cache/huggingface/transformers/
 MOUNTS["${ROOT}/models/CompVis/stable-diffusion-safety-checker"]=/data/.cache/huggingface/transformers/
+MOUNTS["${ROOT}/models/ldm"]=/data/.cache/invoke/ldm/
 
 MOUNTS["${ROOT}/embeddings"]=/data/embeddings/
 
 # hacks
-MOUNTS["${ROOT}/models/clipseg"]=/data/.cache/invoke/clipseg/
 
 for to_path in "${!MOUNTS[@]}"; do
   set -Eeuo pipefail
@@ -40,7 +41,9 @@ done
 
 if "${PRELOAD}" == "true"; then
   set -Eeuo pipefail
-  # invokeai-configure --skip-sd-weights --root ${ROOT} --yes
+  invokeai-configure --root ${ROOT} --yes
 fi
-sleep 100
+
+sed -i 's|chunksize=4096|chunksize=16777216|' ${ROOT}/invokeai/backend/model_management/model_manager.py
+
 exec "$@"
