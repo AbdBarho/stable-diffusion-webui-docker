b8c9e4609151a21dad0bd52dfa847c423a096ed7
AbdBarho <ka70911@gmail.com>
2023-04-10 11:41:31 +0200

    simplify

--- services/invoke/entrypoint.sh
+++ services/invoke/entrypoint.sh
@@ -4,10 +4,12 @@ set -Eeuo pipefail
 
 declare -A MOUNTS
 
+mkdir -p ${CONFIG_DIR}
+
 # cache
 MOUNTS["/root/.cache"]=/data/.cache/
 
-# this is really a hack to avoid migrations
+# this is really just a hack to avoid migrations
 rm -rf ${HF_HOME}/diffusers
 
 # ui specific
@@ -16,9 +18,6 @@ MOUNTS["${ROOT}/models/gfpgan/GFPGANv1.4.pth"]=/data/GFPGAN/GFPGANv1.4.pth
 MOUNTS["${ROOT}/models/gfpgan/weights"]=/data/GFPGAN/
 MOUNTS["${ROOT}/models/realesrgan"]=/data/RealESRGAN/
 
-MOUNTS["${ROOT}/models/bert-base-uncased"]=/data/.cache/huggingface/transformers/
-MOUNTS["${ROOT}/models/openai/clip-vit-large-patch14"]=/data/.cache/huggingface/transformers/
-MOUNTS["${ROOT}/models/CompVis/stable-diffusion-safety-checker"]=/data/.cache/huggingface/transformers/
 MOUNTS["${ROOT}/models/ldm"]=/data/.cache/invoke/ldm/
 
 MOUNTS["${ROOT}/embeddings"]=/data/embeddings/
@@ -42,8 +41,7 @@ done
 if "${PRELOAD}" == "true"; then
   set -Eeuo pipefail
   invokeai-configure --root ${ROOT} --yes
+  cp ${ROOT}/configs/models.yaml ${CONFIG_DIR}/models.yaml
 fi
-
-sed -i 's|chunksize=4096|chunksize=16777216|' ${ROOT}/invokeai/backend/model_management/model_manager.py
 
 exec "$@"
