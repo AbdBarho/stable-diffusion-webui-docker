4d05480e640f3e9f3a92ae77e6c8c8e41e6559b4
PassiveLemon <lemonl3mn@protonmail.com>
2023-04-17 15:27:40 -0400

    Merge + update
    Merges the new changes from the master with some modifications to make Comfy follow the OCI compliance

--- services/AUTOMATIC1111/entrypoint.sh
+++ services/AUTOMATIC1111/entrypoint.sh
@@ -35,6 +35,8 @@ MOUNTS["${ROOT}/models/torch_deepdanbooru"]="/data/Deepdanbooru"
 MOUNTS["${ROOT}/models/BLIP"]="/data/BLIP"
 MOUNTS["${ROOT}/models/midas"]="/data/MiDaS"
 MOUNTS["${ROOT}/models/Lora"]="/data/Lora"
+MOUNTS["${ROOT}/models/ControlNet"]="/data/ControlNet"
+MOUNTS["${ROOT}/models/openpose"]="/data/openpose"
 
 MOUNTS["${ROOT}/embeddings"]="/data/embeddings"
 MOUNTS["${ROOT}/config.json"]="/data/config/auto/config.json"
