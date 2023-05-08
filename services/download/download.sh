24f3263c5a4576d5cd8d9fa4dd9e78fc5c39d06a
AbdBarho <ka70911@gmail.com>
2023-05-08 19:15:52 +0200

    Add ClipEncoder

--- services/download/download.sh
+++ services/download/download.sh
@@ -3,7 +3,27 @@
 set -Eeuo pipefail
 
 # TODO: maybe just use the .gitignore file to create all of these
-mkdir -vp /data/.cache /data/StableDiffusion /data/LyCORIS /data/Codeformer /data/ModelScope /data/GFPGAN /data/ESRGAN /data/BSRGAN /data/RealESRGAN /data/SwinIR /data/LDSR /data/ScuNET /data/embeddings /data/VAE /data/Deepdanbooru /data/MiDaS /data/Lora /data/ControlNet /data/openpose /data/GLIGEN
+mkdir -vp /data/.cache \
+  /data/StableDiffusion \
+  /data/LyCORIS \
+  /data/Codeformer \
+  /data/ModelScope \
+  /data/GFPGAN \
+  /data/ESRGAN \
+  /data/BSRGAN \
+  /data/RealESRGAN \
+  /data/SwinIR \
+  /data/LDSR \
+  /data/ScuNET \
+  /data/embeddings \
+  /data/VAE \
+  /data/Deepdanbooru \
+  /data/MiDaS \
+  /data/Lora \
+  /data/ControlNet \
+  /data/openpose \
+  /data/GLIGEN \
+  /data/CLIPEncoder # this is different from the cached pre-train ViT saved in /data/.cache/clip TODO: find out how
 
 echo "Downloading, this might take a while..."
 
