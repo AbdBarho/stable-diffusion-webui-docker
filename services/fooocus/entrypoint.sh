77cfe4965ee3ccf40634f1be3486da640497c2ca
Paul Lamb <plamb@outlook.com>
2023-12-30 22:20:58 -0800

    Adding mount points to shared models

--- services/fooocus/entrypoint.sh
+++ services/fooocus/entrypoint.sh
@@ -4,22 +4,14 @@ set -Eeuo pipefail
 
 declare -A MOUNTS
 
-#mkdir -p ${CONFIG_DIR} ${ROOT}/configs/stable-diffusion/
+MOUNTS["${ROOT}/outputs"]="/output/fooocus"
 
-# cache
-#MOUNTS["/root/.cache"]=/data/.cache/
-
-# this is really just a hack to avoid migrations
-#rm -rf ${HF_HOME}/diffusers
-
-# ui specific
-#MOUNTS["${ROOT}/models/codeformer"]=/data/models/Codeformer/
-#MOUNTS["${ROOT}/models/gfpgan/GFPGANv1.4.pth"]=/data/models/GFPGAN/GFPGANv1.4.pth
-#MOUNTS["${ROOT}/models/gfpgan/weights"]=/data/models/GFPGAN/
-#MOUNTS["${ROOT}/models/realesrgan"]=/data/models/RealESRGAN/
-#MOUNTS["${ROOT}/models/ldm"]=/data/.cache/invoke/ldm/
-
-# hacks
+# ui specific mounts
+MOUNTS["${ROOT}/models/checkpoints"]=/data/models/Stable-diffusion/
+MOUNTS["${ROOT}/models/loras"]=/data/models/Lora/
+MOUNTS["${ROOT}/models/embeddings"]=/data/models/embeddings/
+MOUNTS["${ROOT}/models/vae_approx"]=/data/models/VAE/
+MOUNTS["${ROOT}/models/upscale_models"]=/data/models/upscale_models/
 
 for to_path in "${!MOUNTS[@]}"; do
   set -Eeuo pipefail
